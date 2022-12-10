7cddab00f8283b0edf06eb481ab6869267962955
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2022-12-11 01:51:40 +0530

    added-basic-template-of-ejs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('working...')
+const express = require("express");
+const app = express();
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('base',{title:"Login Application"})
+})
+
+app.listen(2020,()=>{
+    console.log("Services is listening at PORT:2020")
+})
